4e040d906b6282f9db2464143999ee8e7931627f
GeorgePow <georgeyp1313@gmail,com>
2018-12-19 07:24:08 +0400

    Try move pawns

--- Simpled.py
+++ Simpled.py
@@ -4,24 +4,32 @@ from PyQt5.QtWidgets import QLabel, QLineEdit
 from PyQt5.QtGui import QPainter, QColor, QPen, QFont
 from PyQt5.QtCore import Qt
 
+
 WHITE = 0
 RED = 1
 
+
+def opponent(color):
+    if color == WHITE:
+        return RED
+    else:
+        return WHITE
+    
 
 class Shashki(QWidget):
 
     def __init__(self):
         super().__init__()
         self.header = 'Приветствуем!'
-        # self.current, self.new_coords = None, None # координаты хода
-        while True:
-            self.board = Board()
-            if self.board.current_player_color() == WHITE:
-                self.header = self.header + ' Ход белых:'
-            else:
-                self.header = self.header + ' Ход красных:'
+        self.coords_from, self.coords_to = None, None # координаты хода
+        # while True:
+        self.board = Board()
+        if self.board.currentPlayerColor() == WHITE:
+            self.header = self.header + ' Ход белых:'
+        else:
+            self.header = self.header + ' Ход красных:'
 
-            self.initUI()
+        self.initUI()
 
     def initUI(self):
         self.setGeometry(300, 100, 500, 610)
@@ -33,23 +41,11 @@ class Shashki(QWidget):
         
         self.show()
 
-    def run(self):
-        try:
-            x, y = int(self.col_from.text()), int(self.row_from.text())
-            x1, y1 = int(self.col_to.text()), int(self.row_to.text())
-            if can_move(x, y, x1, y1):
-                self.current = (x, y)
-                self.new_coords = (x1, y1)
-        except:
-            pass
-
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         
         self.drawField(event, qp)
-        self.drawPawn(qp)
-        self.movePawn(qp)
 
         qp.end()
 
@@ -75,18 +71,26 @@ class Shashki(QWidget):
                 qp.drawRect(50 * x, 50 * y, 50, 50)
             first_y = (first_y + 1) % 2
 
-    def drawPawn(self, qp):
         # draw red pawns
-        qp.setPen(Qt.black)
-        qp.setBrush(Qt.red)
-        for x, y in self.red_pawns:
-            qp.drawEllipse(50 * x + 2, 50 * y + 2, 46, 46)
+        for row in range(8):
+            for col in range(8):
+                color = self.board.cell(row, col)
+                qp.setPen(color)
+                qp.setBrush(color)
+                qp.drawEllipse(50 * (col + 1) + 3, 50 * (row + 1) + 3, 44, 44)
 
-        # draw white pawns
-        qp.setPen(Qt.black)
-        qp.setBrush(Qt.white)
-        for x, y in self.white_pawns:
-            qp.drawEllipse(50 * x + 2, 50 * y + 2, 46, 46)
+    def mouseMoveEvent(self, event):
+        if self.coords_from is None:
+            self.coords_from = (event.y() // 50 - 1, event.x() // 50 - 1)
+        else:
+            self.coords_to = (event.y() // 50 - 1, event.x() // 50 - 1)
+            if self.board.movePiece(*self.coords_from, *self.coords_to):
+                self.header = 'Ход успешен!'
+            else:
+                self.header = 'Координаты некорректы! Попробуйте другой ход!'
+            self.coords_from, self.coords_to = None, None
+            
+            self.initUI()
 
     def movePawn(self, qp):
         if self.current and self.new_coords:
@@ -113,50 +117,77 @@ class Board:
         self.field = []
         for row in range(8):
             self.field.append([None] * 8)
-        self.field[0] = [[Pawn(WHITE), None] * 4]
-        self.field[1] = [[None, Pawn(WHITE)] * 4]
-        self.field[2] = [[Pawn(WHITE), None] * 4]
-        self.field[5] = [[[None, Pawn(RED)] * 4]]
-        self.field[6] = [[Pawn(RED), None] * 4]
-        self.field[7] = [[[None, Pawn(RED)] * 4]]
+        self.field[0] = [Pawn(WHITE), None] * 4
+        self.field[1] = [None, Pawn(WHITE)] * 4
+        self.field[2] = [Pawn(WHITE), None] * 4
+        self.field[5] = [None, Pawn(RED)] * 4
+        self.field[6] = [Pawn(RED), None] * 4
+        self.field[7] = [None, Pawn(RED)] * 4
 
-    def current_player_color(self):
+    def currentPlayerColor(self):
         return self.color
 
     def cell(self, row, col):
         piece = self.field[row][col]
         if piece is None:
             return Qt.transparent
-        color = piece.get_color()
+        color = piece.getColor()
         c = Qt.white if color == WHITE else Qt.red
         return c
+
+    def getPiece(self, row, col):
+        if correct_coords(row, col):
+            return self.field[row][col]
+        else:
+            return None
+
+    def movePiece(self, row, col, row1, col1):
+        '''Переместить фигуру из точки (row, col) в точку (row1, col1).
+        Если перемещение возможно, метод выполнит его и вернёт True.
+        Если нет --- вернёт False'''
+
+        if not correct_coords(row, col) or not correct_coords(row1, col1):
+            return False
+        if row == row1 and col == col1:
+            return False  # нельзя пойти в ту же клетку
+        piece = self.field[row][col]
+        if piece is None:
+            return False
+        if piece.getColor() != self.color:
+            return False
+        else:
+            return False
+        self.field[row][col] = None  # Снять фигуру.
+        self.field[row1][col1] = piece  # Поставить на новое место.
+        self.color = opponent(self.color)
+        return True
 
 
 class Pawn:
     def __init__(self, color):
         self.color = color
 
-    def get_color(self):
+    def getColor(self):
         return self.color
 
-    def can_move(self, board, col_from, row_from, col_to, row_to):
-        if abs(col_from - col_to) == 1:
-            return True
-        
+    def canMove(self, board, col_from, row_from, col_to, row_to):
         if abs(col_from - col_to) != abs(row_from - row_to) \
-           or abs(col_from - col_to) > 2:
+           or abs(col_from - col_to) >= 2:
             return False
 
+        if abs(col_from - col_to) == 1:
+            return True
+      
         return False
 
     
 def correct_coords(row, col):
     '''Функция проверяет, что координаты (row, col) лежат
     внутри доски'''
-    return 1 <= row <= 8 and 1 <= col <= 8
+    return 0 <= row < 8 and 0 <= col < 8
 
 
-def can_move(col_from, row_from, col_to, row_to):
+def canMove(col_from, row_from, col_to, row_to):
     if not correct_coords(row_from, col_from) or \
        not correct_coords(row_to, col_to):
         return False
